3ab6374e87e6068132f93b0a533dc5479b611758
angeliapgb <angeliapgb@gmail.com>
2021-06-15 15:39:20 +0700

    commit

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-// function setMessage(formElement, type, message) {
-//     const messageText = formElement.querySelector(".form-message");
-//     messageText.textContent = message;
-//     messageText.classList.remove("form-message-success", "form-message-error");
-//     messageText.classList.add('form-message-${type}');
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const loginForm = document.querySelector("#login");
-//     const signupForm = document.querySelector("#signup");
-
-//     loginForm.addEventListener("submit", e => {
-//         e.preventDefault();
-
-//         setMessage(loginForm, "error", "Invalid username or password!");
-//     });
-// })
-
-// question & answer
+// List questions
 var questionList = [
     {
         question: "Ruangan apa yang paling ditakuti di rumah sakit?",
@@ -41,132 +23,77 @@ var questionList = [
     }
 ]
 
+// Declaration button
 const buttonCheck = document.getElementById('check');
 const buttonNext = document.getElementById('next-question');
 const buttonPrev = document.getElementById('prev-question');
 
+// Declaration error & hint
 const errorMessage = document.getElementById('error-message');
 const hintMessage = document.getElementById('hint-message');
 
+// Declaration hint button
 const hintButton = document.getElementById('hint');
 const hintMsg = document.getElementById('hint-text');
 
-// const buttonNextQuestion = document.getElementById('next');
-
+// Declaration temp variable
 var questionContent = document.getElementById('question');
 var currentQuestion = 0;
 var index = 0;
 
-// hintButton.addEventListener('click', prevention());
-
-// function prevention() {
-//     return false;
-// }
-
-// buttonCheck.addEventListener('click', answerCheck);
-// buttonNext.addEventListener('click', showQuestion(currentQuestion+1));
-// buttonPrev.addEventListener('click', showQuestion(currentQuestion-1));
-
-// buttonNextQuestion.addEventListener('click', showQuestion(currentQuestion+1));
-
-function showNext() {
-    showQuestion(currentQuestion + 1);
-    return false;
-}
-
-function showPrev() {
-    showQuestion(currentQuestion - 1);
-    return false;
-}
-
-// Check answer
-function answerCheck(e) {
-    e.preventDefault();
-    var input = document.forms["user-input"]["user-answer"].value;
-
-    // buttonNext.addEventListener('click', showQuestion(currentQuestion+1));
-
-    for(index = currentQuestion; index <= questionList.length + 1; index++) {
-        
-        // if(buttonNext.onclick == true) {
-        //     showNext();
-        // } else if(buttonPrev.onclick == true) {
-        //     showQuestion(currentQuestion - 1)
-        // }
-        
-        if(input == questionList[index].answer && questionContent.textContent == questionList[index].question) {
-            document.getElementById('error-message').style.visibility = "hidden";
-            hintMessage.style.display = "block";
-            errorMessage.style.display = "none";
-            hintMsg = questionList[index].hintAnswer;
-            currentQuestion += 1;
-            index += 1;
-            if ( index == questionList.length) {
-                alert('Easy level completed');
-            }
-            showQuestion(currentQuestion);
-            level(currentQuestion+1);
-            return;
-        } else {
-            hintMessage.style.display = "none";
-            errorMessage.style.display = "block";
-            return;
-        }
-
-        // if(buttonNext == true) {
-        //     showQuestion(currentQuestion + 1);
-        // } else {
-        //     showQuestion(currentQuestion - 1);
-        // }
-    }
-}
-
-function prevNext() {
-    for(index = currentQuestion; index <= questionList.length + 1; index++) {
-        if(buttonNext == true) {
-            showQuestion(currentQuestion + 1);
-        } else {
-            showQuestion(currentQuestion - 1);
-        }
-    }
-}
-
-// Show question
+// Function show question
 function showQuestion(indexQuestion) {
     var index = questionList[indexQuestion];
-
-    questionContent.textContent = index.question; 
+    questionContent.textContent = index.question;
 }
 
+// Display first question
 showQuestion(currentQuestion);
 
+// Display next question
+function showNext() {
+    showQuestion(currentQuestion += 1);
+    errorMessage.style.display = "none";
+    hintMessage.style.display = "none";
+    return currentQuestion;
+}
 
-//level
-function level (currentQuestion) {
-    document.getElementById('level').innerHTML = 'Level ' + currentQuestion;
+// Display prev question
+function showPrev() {
+    showQuestion(currentQuestion -= 1);
+    errorMessage.style.display = "none";
+    hintMessage.style.display = "none";
+    return currentQuestion;
+}
+
+// Function show level
+function level (currentLevel) {
+    document.getElementById('level').innerHTML = 'Level ' + currentLevel;
 }
 
 level(currentQuestion+1);
 
+// Function check answer
+function answerCheck() {
+    var input = document.forms["user-input"]["user-answer"].value;
 
-hintButton.addEventListener('click', hint(currentQuestion));
-function hint (event, currentQuestion) {
-    event.stopPropagation();
-    hintMsg = hintAnswer[currentQuestion];
+    for(index = currentQuestion; index <= questionList.length + 1; index++) {
+        if(input == questionList[index].answer && questionContent.textContent == questionList[index].question) {
+            hintMessage.style.display = "block";
+            errorMessage.style.display = "none";
+            currentQuestion += 1;
+            index += 1;
+            if(index == questionList.length) {
+                alert('Easy level completed');
+            }
+            showQuestion(currentQuestion);
+            level(currentQuestion += 1);
+            return false;
+        } else {
+            hintMessage.style.display = "none";
+            errorMessage.style.display = "block";
+            return false;
+        }
+    }
+    return false;
 }
-
-// document.getElementById('level').innerHTML = 'Level ' + (currentQuestion+1);
-
-// const username  = document.getElementById('username');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-// const signup = document.getElementById('signup');
-
-// signup.addEventListener('click', checkInput);
-
-// function checkInput() {
-//     const usernameValue = username.value;
-//     const emailValue = email.value;
-//     const passwordValue = password.value;
-    
-// }
