005e852390385a96dbfd9975ae09f3fc8765ee86
angeliapgb <angeliapgb@gmail.com>
2021-06-15 14:14:32 +0700

    commit

--- main.js
+++ main.js
@@ -20,19 +20,24 @@
 var questionList = [
     {
         question: "Ruangan apa yang paling ditakuti di rumah sakit?",
-        answer: "administrasi"
+        answer: "administrasi",
+        hintAnswer: "Tempat membayar"
     }, {
         question: "Binatang apa yang seluruh anggota tubuhnya ada di kepala?",
-        answer: "kutu rambut"
+        answer: "kutu rambut",
+        hintAnswer: "Hitam kecil"
     }, {
         question: "Aku selalu ada di atas presiden dan menteri, tapi aku tidak punya jabatan apa pun dalam pemerintahan. Siapakah aku?",
-        answer: "peci"
+        answer: "peci",
+        hintAnswer: "Bundar"
     }, {
         question: "Siapa yang selalu jadi korban pemerasan?",
-        answer: "sapi perah"
+        answer: "sapi perah",
+        hintAnswer: "Bundar"
     }, {
         question: "Hewan apa yang paling kurang ajar?",
-        answer: "kutu rambut"
+        answer: "kutu rambut",
+        hintAnswer: "Hitam kecil"
     }
 ]
 
@@ -43,44 +48,90 @@ const buttonPrev = document.getElementById('prev-question');
 const errorMessage = document.getElementById('error-message');
 const hintMessage = document.getElementById('hint-message');
 
+const hintButton = document.getElementById('hint');
+const hintMsg = document.getElementById('hint-text');
+
 // const buttonNextQuestion = document.getElementById('next');
 
 var questionContent = document.getElementById('question');
 var currentQuestion = 0;
 var index = 0;
 
-buttonCheck.addEventListener('click', answerCheck);
+// hintButton.addEventListener('click', prevention());
+
+// function prevention() {
+//     return false;
+// }
+
+// buttonCheck.addEventListener('click', answerCheck);
 // buttonNext.addEventListener('click', showQuestion(currentQuestion+1));
 // buttonPrev.addEventListener('click', showQuestion(currentQuestion-1));
 
-
 // buttonNextQuestion.addEventListener('click', showQuestion(currentQuestion+1));
 
+function showNext() {
+    showQuestion(currentQuestion + 1);
+    return false;
+}
+
+function showPrev() {
+    showQuestion(currentQuestion - 1);
+    return false;
+}
+
+// Check answer
 function answerCheck(e) {
     e.preventDefault();
     var input = document.forms["user-input"]["user-answer"].value;
 
+    // buttonNext.addEventListener('click', showQuestion(currentQuestion+1));
+
     for(index = currentQuestion; index <= questionList.length + 1; index++) {
-        // document.getElementById('error-message').style.visibility = "hidden";
+        
+        // if(buttonNext.onclick == true) {
+        //     showNext();
+        // } else if(buttonPrev.onclick == true) {
+        //     showQuestion(currentQuestion - 1)
+        // }
+        
         if(input == questionList[index].answer && questionContent.textContent == questionList[index].question) {
             document.getElementById('error-message').style.visibility = "hidden";
             hintMessage.style.display = "block";
-            errorMessage.style.visibility = "hidden";
+            errorMessage.style.display = "none";
+            hintMsg = questionList[index].hintAnswer;
             currentQuestion += 1;
             index += 1;
             if ( index == questionList.length) {
                 alert('Easy level completed');
             }
             showQuestion(currentQuestion);
+            level(currentQuestion+1);
             return;
         } else {
-            hintMessage.style.visibility = "hidden";
+            hintMessage.style.display = "none";
             errorMessage.style.display = "block";
             return;
+        }
+
+        // if(buttonNext == true) {
+        //     showQuestion(currentQuestion + 1);
+        // } else {
+        //     showQuestion(currentQuestion - 1);
+        // }
+    }
+}
+
+function prevNext() {
+    for(index = currentQuestion; index <= questionList.length + 1; index++) {
+        if(buttonNext == true) {
+            showQuestion(currentQuestion + 1);
+        } else {
+            showQuestion(currentQuestion - 1);
         }
     }
 }
 
+// Show question
 function showQuestion(indexQuestion) {
     var index = questionList[indexQuestion];
 
@@ -95,7 +146,14 @@ function level (currentQuestion) {
     document.getElementById('level').innerHTML = 'Level ' + currentQuestion;
 }
 
-level(currentQuestion+1); mh
+level(currentQuestion+1);
+
+
+hintButton.addEventListener('click', hint(currentQuestion));
+function hint (event, currentQuestion) {
+    event.stopPropagation();
+    hintMsg = hintAnswer[currentQuestion];
+}
 
 // document.getElementById('level').innerHTML = 'Level ' + (currentQuestion+1);
 
